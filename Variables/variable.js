f7964644048f1cdcc0c26dcea46d405656bf1a30
SalmanKhan006 <salmankhan8245363@gmail.com>
2026-02-23 16:36:55 +0500

    Variables

---
+++ Variables/variable.js
@@ -0,0 +1,16 @@
+// console.log("Hello World")
+// document.getElementById("heading").innerHTML = "Salman";
+
+//Declaration of Variable:
+// var firstName;
+
+// //Initialization of Variable:
+// firstName = "salman";
+// let secondName = "khan"
+
+// console.log(firstName);
+// console.log(secondName);
+
+const firstName = "Salman";
+// firstName = "Khan";
+console.log(firstName);
