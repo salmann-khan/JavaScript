c0ec4b487aa39ac3d2969b84263c50b283c54c3f
SalmanKhan006 <salmankhan8245363@gmail.com>
2026-02-23 16:37:54 +0500

    Data Types & Operators

---
+++ Data Types and Operators/Operators and Data Types.js
@@ -0,0 +1,87 @@
+// /* 
+// Operand: data on which operatins are performed.
+// Operator: type of operation.
+// Expression: combination of operator and operands.
+// e.g:
+// b + c;  -> b and c are operands, + indicates operator.
+
+// */
+
+// let x = 4;
+// let y = 5;
+// let z;
+
+// // z = x + y;
+// // z = x - y;
+// // z = x * y;
+// // z = x / y;
+// // z = x % y;
+// // console.log(z);
+
+
+// // x = x + y;
+// //shorthand property of above line
+// /*x*=y;
+// x/=y;
+// x-=y;
+// x+=y;
+
+// console.log(x); */
+
+// // power operator
+// // z = x**3;   
+// // console.log(z);
+
+
+// // Increment & Decrement Operator
+// //  x++;   //Add 1
+// //  console.log(x);
+// //  x--;  //Subtract 1
+// //  console.log(x);
+// // y= "2";
+// //concatenation-> combine
+// //console.log(x+y); int and strings can't be added, but combined.
+// //console.log(x+" "+y); adds x with space then adds with y.
+
+
+// //bodmas rule
+// // z= (x+2)/2;
+// // console.log(z);
+
+// //  ******* Data Types ******** //
+
+// let x = 2;
+// let y;
+// y = 'Hel"lo';
+// // y = "Hel'lo";
+// // y = "Hello";
+// // y = "Hel\"lo";
+// // y = 'Hel\'lo';
+
+// console.log(y);
+
+
+// //Boolean
+// let z = 5;
+// let y = 4;
+// let x;
+// x=z>y;
+// console.log(x);
+
+//undefined -> variable isn't initialized
+// let z;
+// console.log(z);
+
+// let z = typeof "salman";
+// let y = typeof 78;
+// console.log(z);
+// console.log(y);
+
+// Template Literal
+let x = "Hi";
+let y = "World";
+
+//tild operator: `
+console.log(`${x} Salman`);  //${x}-> template literal
+// let z = x +" "+ y;  //concatenation
+// console.log(z);
