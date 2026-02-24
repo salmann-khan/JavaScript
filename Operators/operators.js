b52b993931ebb882c9505536b77f16deddb89f16
SalmanKhan006 <salmankhan8245363@gmail.com>
2026-02-24 15:37:49 +0500

    Operators

---
+++ Operators/operators.js
@@ -0,0 +1,18 @@
+/*
+Comparison Operator
+Compare
+1) ==   -> equal to
+2) ===  -> checks number and also data type
+3) !=   -> Not equals to
+4) !==  -> Not equals and not equal data type
+5) greater than >
+6) less <
+7) greater than equals to >=
+8) less than equals to <=
+
+//////// LOGICAL OPERATORS //////////
+1) And &&  All values must be true
+2) OR ||  Atleast one value must be true
+3) NOT ! Opposites the value
+
+*/ 
