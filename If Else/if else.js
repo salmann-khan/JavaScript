73ff3bb725134803fdffb12e04d4b073ea4dc7ea
SalmanKhan006 <salmankhan8245363@gmail.com>
2026-02-24 16:12:22 +0500

    If Else

--- If Else/if else.js
+++ If Else/if else.js
@@ -41,6 +41,8 @@ else{
 
 TERNIARY OPERATOR:
 variable(condition) ? {output if the value is true} : {output if the value is false}
+
+
 Nullish Coalescing Operator:
 syntax:
 valueToCheck ?? defaultValue;
@@ -57,14 +59,58 @@ let number = 4;
 
 // Nested IF
 
-if(number>3){
-    if(number>7){
-        document.write("Number is greater than 3 and 7");
-    }
-    else{
-        document.write("Number is less than 7");
-    }
-}
-else{
-    document.write("Number is less than 3");
-}
+// if(number>3){
+//     if(number>7){
+//         document.write("Number is greater than 3 and 7");
+//     }
+//     else{
+//         document.write("Number is less than 7");
+//     }
+// }
+// else{
+//     document.write("Number is less than 3");
+// }
+
+// IF ELSE IF LADDER
+// number = 3;
+// if(number>0 && number<5){
+//     document.write("Number is Positive");
+// }
+// else if(number==0){
+//     document.write("Number is 0");
+// }
+// else{
+//     document.write("Number is Negative");
+// }
+
+
+
+// number = 8;
+
+// if(number>0 && number<5){
+//     document.write("IF CONDITION");
+// }
+// else if(number>5 && number<7){
+//     document.write("ELSE IF");
+// }
+// else if(number>7 && number<9){
+//     document.write("ELSE IF 2");
+// }
+// else{
+//     document.write("ELSE");
+// }
+
+
+// TERNARY OPERATOR
+
+// let result;
+// result = 0;  
+// let status = result == 1 ? "Pass" : "Fail";
+// document.write(status);
+
+//Nullish Operator
+
+let firstName;
+let defaultName = "Guest";
+let result = firstName ?? defaultName;
+document.write(result);
