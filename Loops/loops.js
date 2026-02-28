c1d0768234228746416f8f48b7ef97c698fa53c4
SalmanKhan006 <salmankhan8245363@gmail.com>
2026-02-28 23:42:09 +0500

    Loops

---
+++ Loops/loops.js
@@ -0,0 +1,15 @@
+//Loops
+//While:
+// let number = 0;
+
+// while(number<10)
+// {
+//     document.write(number);
+//     number++;
+// }
+let message = 0;
+do{
+    document.write(`Hello Salman ${message}`);
+    message++;
+}
+while(message<10);
