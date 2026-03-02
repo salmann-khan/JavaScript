6c9428c475cdb5eb1842d24e20760ac1d09f26d8
SalmanKhan006 <salmankhan8245363@gmail.com>
2026-03-02 16:29:05 +0500

    alert

---
+++ Alert Promt Confirm/alert.js
@@ -0,0 +1,11 @@
+// Alert Prompt Confirm --> Functions
+/*
+alert() -> display a message
+
+prompt() -> show a message, a input textbox, it returns the input on ok or, if cancel button or ESC button it returns null;
+
+confirm() -> show a message to confirm, returns true when ok is clicked and return false when cancel/esc is clicked
+ */
+
+alert("You are loggedIn!");
+alert("Welcome");
