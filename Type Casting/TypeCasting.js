b663cfa04a2eddee8d32cbee1b270000c9b7222e
SalmanKhan006 <salmankhan8245363@gmail.com>
2026-03-03 18:30:46 +0500

    TypeOf

--- Type Casting/TypeCasting.js
+++ Type Casting/TypeCasting.js
@@ -16,6 +16,31 @@
 // document.writeln(typeof number);
 
 
-let number = "5" * 2;
-document.writeln(number);
-document.writeln(typeof number);
+// let number = "5" * 2;
+// document.writeln(number);
+// document.writeln(typeof number);
+
+/**
+ * Types
+ 1)String()
+ 2)Number()
+ 3)Boolean()
+ */
+
+//  let oldType = 2;
+//  document.writeln(typeof oldType);
+
+//  let newType = String(oldType);
+//  document.writeln(typeof newType);
+
+//  let oldType = "42";
+//  document.writeln(typeof oldType);
+
+//  let newType = Number(oldType);
+//  document.writeln(typeof newType);
+
+ let oldType = "42";
+ document.writeln(typeof oldType);
+
+ let newType = Boolean(oldType);
+ document.writeln(typeof newType);
