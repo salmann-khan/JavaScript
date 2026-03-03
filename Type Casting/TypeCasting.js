f939d5de3efba68b14860f2031997787a7d2feb0
SalmanKhan006 <salmankhan8245363@gmail.com>
2026-03-03 18:17:24 +0500

    TypeOf

---
+++ Type Casting/TypeCasting.js
@@ -0,0 +1,11 @@
+//******** TYPE CASTING *********/
+
+// let number = "4";
+// document.writeln(number);
+// document.writeln(typeof number);
+
+
+
+let number = 4 + null;
+document.writeln(number);
+document.writeln(typeof number);
