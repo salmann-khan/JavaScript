d0b77cff6bce1d53d1b43558c71703400c53d3c6
SalmanKhan006 <salmankhan8245363@gmail.com>
2026-03-06 17:55:13 +0500

    Names

--- String Manipulation/string.js
+++ String Manipulation/string.js
@@ -1,5 +1,24 @@
 //String Manipulation
 
 
-let message = "This is message";
-console.log(message);
+// let message = "This is \nmessage";
+// let message = "This is \tmessage";
+// let message = "This is \\ message";
+// let message = "This is\" message";
+// console.log(message.length);
+// let message = "This is message";
+// console.log(message[10]);
+
+// let firstName = "Salman";
+// let lastName = "Khan";
+// console.log(firstName +" "+ lastName);
+
+let name = "Salman";
+if(name == "Salman")
+{
+    console.log("Equal");
+
+}
+else{
+    console.log("Not Equals");
+}
