fc5ac1b10e16303854ad282fb0a765f07f780aa1
SalmanKhan006 <salmankhan8245363@gmail.com>
2026-03-07 18:08:39 +0500

    includes

--- String Manipulation/string.js
+++ String Manipulation/string.js
@@ -35,9 +35,9 @@ let message = "   This is message   ";
 // let result = message.lastIndexOf('e');
 // let result = message.trimEnd();
 
-// let result = message.toUpperCase();
-// let result = message.toLowerCase();
+let result = message.toUpperCase();
+let result = message.toLowerCase();
 
-// // let result = message.replace("is","iss");
-// let result = message.includes("message");
-// // console.log(result);
+// let result = message.replace("is","iss");
+let result = message.includes("message");
+// console.log(result);
