a490bc5742231c193ea9f28582acdd48e2349c54
Salman Khan <salmankhan8245363@gmail.com>
2026-03-07 18:12:07 +0500

    Update string.js

--- String Manipulation/string.js
+++ String Manipulation/string.js
@@ -39,5 +39,3 @@ let message = "   This is message   ";
 // let result = message.toLowerCase();
 
 // let result = message.replace("is","iss");
-let result = message.includes("message");
-console.log(result);
