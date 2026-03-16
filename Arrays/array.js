d32e0de76e1d5f027100ac6bcccf1b0c94ebc4e6
SalmanKhan006 <salmankhan8245363@gmail.com>
2026-03-16 16:06:47 +0500

    Arrays

--- Arrays/array.js
+++ Arrays/array.js
@@ -1,1 +1,8 @@
-//Arrays
+//Arrays in JavaScript
+//Array: Storing multiple elements in single variable
+
+// let students = ["Salman", "Imran", "Kamran", "Ali", "Ahmed", "Huzaifa"];
+// console.log(students[4]);
+
+// let info = [1, "Ali", 60.60];
+// console.log(info[1]);
