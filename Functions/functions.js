ada364c6281bb482db20f88656f47bac6b64f391
SalmanKhan006 <salmankhan8245363@gmail.com>
2026-03-16 17:21:37 +0500

    Functions

---
+++ Functions/functions.js
@@ -0,0 +1,20 @@
+// Functions in JavaScript
+
+// function greet(){
+//     console.log("Hello I'm Salman Khan");
+// }
+// greet();
+
+// function add(a,b){
+//     console.log(a+b);
+// }
+
+// // calling function
+// add(5,5);
+
+function greet(name)
+{
+    console.log(`Hello ${name}!`);
+}
+let name = prompt("Enter your name: ");
+greet(name);
