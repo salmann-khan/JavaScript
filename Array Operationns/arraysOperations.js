cfa6fb942c2d793cf4d31ee35eb911a863be334e
SalmanKhan006 <salmankhan8245363@gmail.com>
2026-03-16 16:57:32 +0500

    Array Operations

--- Array Operationns/arraysOperations.js
+++ Array Operationns/arraysOperations.js
@@ -1,12 +1,37 @@
 // Array Operations
 
-// let teams = ["Pakistan", "India", "England", "Austrailia"];
+let teams = ["Pakistan", "India", "England", "Austrailia"];
 // console.log(teams.sort());
 
-let teams = ["Pakistan", "India", "England", "Austrailia"];
-teams.push("NewZealand");  // Adds in End
-teams.unshift("Zimbave"); //Adds in Beginning
-// teams.pop();  // Delete the last element
-// teams.shift(); //Delete the first Element
-teams.splice(1, 2); //Removes elements from index 1 to forward 2 indexes
-console.log(teams);
+// let teams = ["Pakistan", "India", "England", "Austrailia"];
+// teams.push("NewZealand");  // Adds in End
+// teams.unshift("Zimbave"); //Adds in Beginning
+// // teams.pop();  // Delete the last element
+// // teams.shift(); //Delete the first Element
+// teams.splice(1, 2); //Removes elements from index 1 to forward 2 indexes
+// /*
+// //teams = [];
+// //teams.length = 0;
+// Empty the array
+// */
+// console.log(teams);
+
+// let teams = "Austrailia";
+// console.log(Array.isArray(teams));
+
+// let message = "Hello There! I'm Salman A Web Developer Jr";
+// let splitting = message.split(" ");
+// console.log(splitting);
+
+// let arr1 = ["Salman ", "Khan ", "Ahmed"];
+// let arr2 = ["Kamran ", "Imran ", "Ali"];
+// let newArr = arr1.concat(arr2);
+// console.log(newArr);
+
+let books = [
+    ["Maths", "Calculus"],
+    ["Bio", "Zoology"],
+    ["Computer", "Ai"]
+];
+let arr = books[1][0];
+console.log(arr);
